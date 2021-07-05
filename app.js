66b4b23822f3c06ae848de78c5b2358345eb2cc0
Aman Singh <ars.arsingh@gmail.com>
2021-07-06 00:02:32 +0530

    add : function clickHandler

---
+++ app.js
@@ -0,0 +1,21 @@
+var inputText = document.querySelector("#input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#output");
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function textTranslate(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function clickHandler() {
+    var textInput = inputText.value;
+    fetch(textTranslate)
+        .then(response => response.json())
+        .then(json => {
+                var translatedText = json.contents.translated;
+                translatedText.innerText;
+            }
+        }
+
+
+    btnTranslate.addEventListener("click", clickHandler);
